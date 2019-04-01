340ba2ea95c328b621a7f4075aec12c680e4e3e6
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2019-04-01 14:05:05 +0200

    Add decode results param to recall evaluation

--- SCN_evaluation_recall.py
+++ SCN_evaluation_recall.py
@@ -115,6 +115,9 @@ def check_args(args):
         help="File containing occurrences statistics about adjective noun pairs",
         required=True,
     )
+    parser.add_argument(
+        "--decode-results", help="Path decoding results", required=True
+    )
 
     parsed_args = parser.parse_args(args)
     print(parsed_args)
@@ -124,7 +127,7 @@ if __name__ == '__main__':
     parsed_args = check_args(sys.argv[1:])
 
     # generated captions
-    generated_captions = pickle.load(open("./decode_results.p", "rb"))
+    generated_captions = pickle.load(open(parsed_args.decode_results, "rb"))
     # generated_captions = {idx: [lines.strip()] for (idx, lines) in enumerate(open('./coco_scn_5k_test.txt', 'rb') )}
     occurrences_data_file = parsed_args.occurrences_data
     coco_ids = [str(key) for key in generated_captions.keys()]
